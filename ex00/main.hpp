618eaf1488eae2effe242cb48523e24a55a656cd
samartin <samartin@student.42madrid.com>
2025-11-13 17:01:28 +0100

    initial scheme

---
+++ ex00/main.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 13:53:06 by samartin          #+#    #+#             */
+/*   Updated: 2025/09/12 14:25:03 by samartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# ifndef TSTL
+#  define TSTL "\e[32m"
+# endif
+# ifndef TSTH
+#  define TSTH "\e[33m\e[44m"
+# endif
+# ifndef TSTR
+#  define TSTR "\e[00m\n"
+# endif
+
+#include <iostream>
+
+#endif
